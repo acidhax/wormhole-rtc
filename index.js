63d7c5e7aa743ec6564780d4295a5297853bad2f
Mathieuu Gosbee <mail@matbee.com>
2014-01-20 16:25:13 -0500

    .

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ var wormholeRTC = function (enableWebcam, enableAudio, enableScreen) {
 	this._audioStream = null;
 
 	if (!RTCPeerConnection && !RTCSessionDescription && !RTCIceCandidate) {
-		setTimeout(function () {
-			self.emit("notsupported", {});
-		}, 100);
+		// setTimeout(function () {
+			self.emit("notsupported");
+		// }, 100);
 	}
 	this.on("newlistener", function (ev, cb) {
 		if (ev == "notsupported") {
