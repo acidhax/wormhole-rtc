151f0e1e48a02d49a12a7d991fde171dcecdad8e
Mathieuu Gosbee <mail@matbee.com>
2014-01-20 15:45:23 -0500

    emit notsupported if webrtc doesnt exst

--- index.js
+++ index.js
@@ -22,6 +22,10 @@ var wormholeRTC = function (enableWebcam, enableAudio, enableScreen) {
 	this._videoStream = null;
 	this._audioStream = null;
 
+	if (RTCPeerConnection || RTCSessionDescription || RTCIceCandidate) {
+		this.emit("notsupported");
+	}
+
 	this.addRTCFunction("handleOffer", function (offerDescription, cb) {
 		console.log("handleOffer", this.id, offerDescription);
 		self.handleOffer(this.id, offerDescription, cb);
